4ad7ea5feb547bab4dcdf0fcbd1b25ab9ad433a7
KuMa <martin.erichsen@gmx.de>
2020-03-11 22:33:52 +0100

    Close windows with esc

--- core/src/main/java/com/kumascave/games/teeth_of_doom/core/ui/actors/windows/DisposableWindow.java
+++ core/src/main/java/com/kumascave/games/teeth_of_doom/core/ui/actors/windows/DisposableWindow.java
@@ -1,5 +1,6 @@
 package com.kumascave.games.teeth_of_doom.core.ui.actors.windows;
 
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.utils.Disposable;
@@ -15,6 +16,18 @@ public class DisposableWindow extends ScalableWindow implements Disposable {
 			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
 				// dont propagate clicks in window
 				return true;
+			}
+
+			@Override
+			public boolean keyDown(InputEvent event, int keycode) {
+				switch (keycode) {
+				case Keys.ESCAPE:
+					DisposableWindow.this.dispose();
+					return true;
+				default:
+					break;
+				}
+				return false;
 			}
 		});
 	}
